667a9ec24608b97bdd68cee54cba0aabe76d3e75
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-05 20:30:51 -0800

    added inquirer prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = []
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				message: 'Enter a title:',
+				name: 'title',
+			},
+			{
+				type: 'checkbox',
+				message: 'Choose a license:',
+				name: 'license',
+				choices: ['MIT', 'GNU', 'Apache', 'Mozilla', 'ISC'],
+			},
+			{
+				type: 'input',
+				message: 'Enter a description:',
+				name: 'description',
+			},
+			{
+				type: 'input',
+				message: 'Enter the usage of this project:',
+				name: 'usage',
+			},
+			{
+				type: 'input',
+				message: 'Enter the installation method:',
+				name: 'installation',
+			},
+		])
+		.then((data) => {
+			const filename = 'README.md'
+
+			fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+				err ? console.log(err) : console.log('Success!')
+			)
+		})
+}
 
 // Function call to initialize app
-init();
+init()
