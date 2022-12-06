618f82740f1cc43cbc36e011039e4f9d20a57eb8
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-05 21:07:52 -0800

    add function to display license badge

--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,30 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-const markdownStructure = (data) =>
-	`
-# ${data.title}
+const fetchLicense = (license) => {
+	switch (license) {
+		case 'MIT':
+			return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+		case 'GNU':
+			return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+		case 'Apache':
+			return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+		case 'ISC':
+			return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+		case 'Mozilla':
+			return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+		default:
+			'No license provided.'
+	}
+}
+
+const generateMarkdown = (data) =>
+	`# ${data.title}
+
+# License 
+
+${fetchLicense(data.license.toString())}
+    
 
 ## Description
 
@@ -51,16 +72,19 @@ const questions = [
 ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+	const filename = 'README.md'
+	fs.writeFile(filename, data, (err) =>
+		err ? console.log(err) : console.log('Success!')
+	)
+}
 
 // TODO: Create a function to initialize app
 function init() {
 	inquirer.prompt(questions).then((data) => {
-		const filename = 'README.md'
-		const finishedMarkup = markdownStructure(data)
-		fs.writeFile(filename, finishedMarkup, (err) =>
-			err ? console.log(err) : console.log('Success!')
-		)
+		console.log(data.license.toString())
+		const completedMarkdownContent = generateMarkdown(data)
+		writeToFile(completedMarkdownContent)
 	})
 }
 
