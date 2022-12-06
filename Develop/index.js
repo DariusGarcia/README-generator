df2f5cd749e64ad00593f357ca3683ab4b25a0bd
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-05 20:42:19 -0800

    added logic to write user input to README

--- Develop/index.js
+++ Develop/index.js
@@ -2,51 +2,69 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+const markdownStructure = (data) =>
+	`
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Usage
+
+${data.usage}
+
+## Installation
+
+${data.installation}
+
+`
+
 // TODO: Create an array of questions for user input
-const questions = []
+const questions = [
+	{
+		type: 'input',
+		message: 'Enter a title:',
+		name: 'title',
+	},
+	{
+		type: 'checkbox',
+		message: 'Choose a license:',
+		name: 'license',
+		choices: ['MIT', 'GNU', 'Apache', 'Mozilla', 'ISC'],
+	},
+	{
+		type: 'input',
+		message: 'Enter a description:',
+		name: 'description',
+	},
+	{
+		type: 'input',
+		message: 'Enter the usage of this project:',
+		name: 'usage',
+	},
+	{
+		type: 'input',
+		message: 'Enter the installation method:',
+		name: 'installation',
+	},
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-	inquirer
-		.prompt([
-			{
-				type: 'input',
-				message: 'Enter a title:',
-				name: 'title',
-			},
-			{
-				type: 'checkbox',
-				message: 'Choose a license:',
-				name: 'license',
-				choices: ['MIT', 'GNU', 'Apache', 'Mozilla', 'ISC'],
-			},
-			{
-				type: 'input',
-				message: 'Enter a description:',
-				name: 'description',
-			},
-			{
-				type: 'input',
-				message: 'Enter the usage of this project:',
-				name: 'usage',
-			},
-			{
-				type: 'input',
-				message: 'Enter the installation method:',
-				name: 'installation',
-			},
-		])
-		.then((data) => {
-			const filename = 'README.md'
-
-			fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-				err ? console.log(err) : console.log('Success!')
-			)
-		})
+	inquirer.prompt(questions).then((data) => {
+		const filename = 'README.md'
+		const finishedMarkup = markdownStructure(data)
+		fs.writeFile(filename, finishedMarkup, (err) =>
+			err ? console.log(err) : console.log('Success!')
+		)
+	})
 }
 
 // Function call to initialize app
 init()
+
+// JSON.stringify(data, null, '\t')
