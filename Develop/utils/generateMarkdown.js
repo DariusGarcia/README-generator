a2ecd82c2453cae3614a3ccaaa7976efc9a843c8
Darius <dariusgarcia888@gmail.com>
2022-12-06 21:40:50 -0800

    Update generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,11 +8,11 @@ function renderLicenseBadge(license) {
 		case 'GNU':
 			return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
 		case 'Apache':
-			return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+			return `[![License](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)](https://opensource.org/licenses/Apache-2.0)`
 		case 'ISC':
 			return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
 		case 'Mozilla':
-			return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+			return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-yellow.svg)](https://opensource.org/licenses/MPL-2.0)`
 		default:
 			'No license provided.'
 	}
